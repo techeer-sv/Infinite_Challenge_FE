7f1b148e625f66bf374e1db0f6d059162b39cc6f
teawon <xodnjsvkvk9@gmail.com>
2024-05-07 23:24:24 +0900

    chore: 스토리북 초기 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:storybook/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
