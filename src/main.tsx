c6bb7d8f3814c9b02bc72e4e733f83f6a5cea514
seunghwan <sun1238799@naver.com>
2024-05-10 20:18:47 +0900

    feat: react-query 캐시추가

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,17 @@ import { BrowserRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+      gcTime: 1000 * 60 * 60 * 24,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
