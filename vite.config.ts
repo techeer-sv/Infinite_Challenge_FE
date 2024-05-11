401fbb93ea511896558c940c1fec2718bc88fed7
TMInstaller <bjdy3000@gmail.com>
2024-05-11 15:50:36 +0900

    chore: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 이렇게 설정되어 있는지 확인
+    },
+  },
+});
