42f5fafc95cd568a67987028e30fa22a67b21555
Yeongjun Joo <wndudwns6824@naver.com>
2024-05-12 18:57:18 +0900

    refactor : 헤더, 검색페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <NavBar />
       <div className="w-full h-[46vh] bg-skyColor flex flex-col justify-center items-center">
         <div className="flex flex-col justify-center items-center text-3xl font-bold">
-          <div>국내 모든 임상시험 검색하고</div>
+          <div className="mb-2">국내 모든 임상시험 검색하고</div>
           <div>온라인으로 참여하기</div>
         </div>
         <SearchBar />
