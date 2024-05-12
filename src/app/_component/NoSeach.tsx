43cf855d02c58b30de4244992ddcda616f1ad254
Yeongjun Joo <wndudwns6824@naver.com>
2024-05-12 18:58:38 +0900

    refactor : NoSearch

--- src/app/_component/NoSeach.tsx
+++ src/app/_component/NoSeach.tsx
@@ -1,6 +1,6 @@
-export default function NoSearchResult() {
+export default function NoSearch() {
   return (
-    <div className="w-[25rem] h-[6rem] fixed top-[21rem] rounded-xl bg-white p-[20px] shadow-lg">
+    <div className="w-[486px] h-[6rem] fixed top-[20.5rem] rounded-xl bg-white p-[20px] shadow-lg">
       <div className="pb-[10px] text-gray-400">최근 검색어</div>
       <div className="text-gray-300">최근 검색어가 없습니다</div>
     </div>
