9c9f20d748b4a39c96af9b92e86496d55560b414
Yeongjun Joo <wndudwns6824@naver.com>
2024-05-12 16:05:59 +0900

    design : 검색결과없음 컴포넌트

---
+++ src/app/_component/NoSeach.tsx
@@ -0,0 +1,8 @@
+export default function NoSearchResult() {
+  return (
+    <div className="w-[25rem] h-[6rem] fixed top-[21rem] rounded-xl bg-white p-[20px] shadow-lg">
+      <div className="pb-[10px] text-gray-400">최근 검색어</div>
+      <div className="text-gray-300">최근 검색어가 없습니다</div>
+    </div>
+  );
+}
