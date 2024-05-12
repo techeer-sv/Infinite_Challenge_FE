e10b8531b496d3dfa949582527eb556463ba8ca7
Yeongjun Joo <wndudwns6824@naver.com>
2024-05-12 23:54:22 +0900

    feat : searchbar 기능 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.clinicaltrialskorea.com/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
