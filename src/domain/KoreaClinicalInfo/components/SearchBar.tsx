6e0ce069db19f4bee9ed995330bde068f9cc655a
teawon <xodnjsvkvk9@gmail.com>
2024-05-13 02:05:53 +0900

    feat: 검색바 퍼블리싱 및 더미 데이터 연결

--- src/domain/KoreaClinicalInfo/components/SearchBar.tsx
+++ src/domain/KoreaClinicalInfo/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import SearchIcon from "@/components/icons/SearchIcon";
 import { useState } from "react";
 import Button from "@/components/Button";
+import SearchSuggestion from "./SearchSuggestion";
 // Controlled 컴포넌트
 interface ISearchBar {
   value: string;
@@ -26,31 +27,38 @@ const SearchBar = ({ value, placeholder, onSubmit, onChange }: ISearchBar) => {
   };
 
   return (
-    <form
-      className={`flex items-center p-2 rounded-full relative border border-solid bg-white ${isFocused ? "border-primary" : "border-none"}`}
-      onSubmit={handleSubmit}
-      style={{ width: "430px", height: "70px" }}
-    >
-      <div className="flex-grow flex items-center pl-4">
-        <SearchIcon size={16} color="silver" />
-        <input
-          value={value}
-          type="text"
-          className="w-full text-base bg-transparent outline-none ml-2"
-          placeholder={placeholder}
-          onChange={onChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-        />
-      </div>
-      <Button
-        type="submit"
-        customStyle="flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-full ml-2"
-        onClick={() => {}}
+    <>
+      <form
+        className={`flex items-center p-2 rounded-full relative border border-solid bg-white ${isFocused ? "border-primary" : "border-none"}`}
+        onSubmit={handleSubmit}
+        style={{ width: "430px", height: "70px" }}
       >
-        <SearchIcon size={21} color="white" />
-      </Button>
-    </form>
+        <div className="flex-grow flex items-center pl-4">
+          <SearchIcon size={16} color="silver" />
+          <input
+            value={value}
+            type="text"
+            className="w-full text-base bg-transparent outline-none ml-2"
+            placeholder={placeholder}
+            onChange={onChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+        </div>
+        <Button
+          type="submit"
+          customStyle="flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-full ml-2"
+          onClick={() => {}}
+        >
+          <SearchIcon size={21} color="white" />
+        </Button>
+      </form>
+      {isFocused && (
+        <div className="absolute" style={{ top: "320px" }}>
+          <SearchSuggestion searchKeyword={value} />
+        </div>
+      )}
+    </>
   );
 };
 
--- src/domain/KoreaClinicalInfo/components/SearchSuggestion/index.tsx
+++ src/domain/KoreaClinicalInfo/components/SearchSuggestion/index.tsx
@@ -1,3 +1,60 @@
 export { default as SearchItem } from "./SearchItem";
 export { default as RecommendSearch } from "./RecommendSearch";
 export { default as RecentSearch } from "./RecentSearch";
+
+import { RecommendSearch, RecentSearch } from ".";
+
+interface ISearchSuggestion {
+  searchKeyword?: string;
+}
+
+const dummyRecentSearch = [
+  {
+    id: 1,
+    name: "최근검색어1",
+  },
+  {
+    id: 2,
+    name: "최근검색어2",
+  },
+  {
+    id: 3,
+    name: "최근검색어3",
+  },
+];
+
+const dummyRecommendSearch = [
+  {
+    id: 1,
+    name: "추천검색어1",
+  },
+  {
+    id: 2,
+    name: "추천검색어2",
+  },
+  {
+    id: 3,
+    name: "추천검색어3",
+  },
+];
+
+const SearchSuggestion = ({ searchKeyword }: ISearchSuggestion) => {
+  return (
+    <div className="rounded-lg p-4 " style={{ width: "438px" }}>
+      {searchKeyword ? (
+        <RecommendSearch
+          searchKeywords={searchKeyword}
+          recommendKeyWords={dummyRecommendSearch}
+          onItemClick={() => {}}
+        />
+      ) : (
+        <RecentSearch
+          recentKeyWords={dummyRecentSearch}
+          onItemClick={() => {}}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SearchSuggestion;
